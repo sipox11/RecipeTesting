92d82099a17aa533f5c49976639abeb6cb7ac437
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-04-21 21:30:12 +0200

    Set and configure recyclerview in main activity to display a list of recipes by title

--- app/src/main/java/com/sipox11/recipe_testing/ui/main/MainActivity.java
+++ app/src/main/java/com/sipox11/recipe_testing/ui/main/MainActivity.java
@@ -2,13 +2,25 @@ package com.sipox11.recipe_testing.ui.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.sipox11.recipe_testing.R;
+import com.sipox11.recipe_testing.data.local.RecipeStore;
 
 public class MainActivity extends AppCompatActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recipes);
+        RecipeStore store = new RecipeStore(this, "recipes");
+        RecipeAdapter adapter = new RecipeAdapter(store);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
 }
