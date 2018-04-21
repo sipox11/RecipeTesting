ba04fcc884f6278928c98b83a13faebdd820d31f
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-04-21 21:31:07 +0200

    Refactor main activity

--- app/src/main/java/com/sipox11/recipe_testing/ui/main/MainActivity.java
+++ app/src/main/java/com/sipox11/recipe_testing/ui/main/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sipox11.recipe_testing.ui.main;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,18 +10,23 @@ import com.sipox11.recipe_testing.R;
 import com.sipox11.recipe_testing.data.local.RecipeStore;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mContext = this;
+        
         RecyclerView recyclerView = findViewById(R.id.recipes);
-        RecipeStore store = new RecipeStore(this, "recipes");
+        RecipeStore store = new RecipeStore(mContext, "recipes");
         RecipeAdapter adapter = new RecipeAdapter(store);
 
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
 
     }
 }
