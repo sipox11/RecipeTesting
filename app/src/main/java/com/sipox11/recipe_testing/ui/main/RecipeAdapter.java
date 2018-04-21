0f9913845429acdc8dbae55f35b79f7a46964a32
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-04-21 21:20:52 +0200

    Add list item layout file

--- app/src/main/java/com/sipox11/recipe_testing/ui/main/RecipeAdapter.java
+++ app/src/main/java/com/sipox11/recipe_testing/ui/main/RecipeAdapter.java
@@ -5,7 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeViewHolder> {
-    
+
     @NonNull
     @Override
     public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
