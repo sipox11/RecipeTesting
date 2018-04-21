648cea31d9226a0abad7683f6e91670cc54360fd
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-04-21 20:00:54 +0200

    Fix compiler issues

--- app/src/main/java/com/sipox11/recipe_testing/ui/main/MainActivity.java
+++ app/src/main/java/com/sipox11/recipe_testing/ui/main/MainActivity.java
@@ -3,7 +3,7 @@ package com.sipox11.recipe_testing.ui.main;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.sqisland.tutorial.recipes.R;
+import com.sipox11.recipe_testing.R;
 
 public class MainActivity extends AppCompatActivity {
   @Override
