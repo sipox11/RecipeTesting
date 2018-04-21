a3855298f9b8dc4985000e760f3b8e845e8d39ca
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-04-21 21:18:52 +0200

    Create recipe adapter skeleton file

---
+++ app/src/main/java/com/sipox11/recipe_testing/ui/main/RecipeAdapter.java
@@ -0,0 +1,24 @@
+package com.sipox11.recipe_testing.ui.main;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeViewHolder> {
+    
+    @NonNull
+    @Override
+    public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
