ca5fa81c23c1c7a3d5ed74090ce4eceb79c93952
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-04-21 21:25:38 +0200

    Implement 3 methods from adapter

--- app/src/main/java/com/sipox11/recipe_testing/ui/main/RecipeAdapter.java
+++ app/src/main/java/com/sipox11/recipe_testing/ui/main/RecipeAdapter.java
@@ -2,23 +2,37 @@ package com.sipox11.recipe_testing.ui.main;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
+import com.sipox11.recipe_testing.R;
+import com.sipox11.recipe_testing.data.local.RecipeStore;
+import com.sipox11.recipe_testing.data.model.Recipe;
+
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeViewHolder> {
+
+    private final RecipeStore store;
+
+    public RecipeAdapter(RecipeStore store) {
+        this.store = store;
+    }
 
     @NonNull
     @Override
     public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new RecipeViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
-
+        final Recipe recipe = store.recipes.get(position);
+        holder.textView.setText(recipe.title);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return store.recipes.size();
     }
 }
