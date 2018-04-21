0b46a0c0022e5e9b253ef94ba5c14e8d619e3c0e
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-04-21 20:38:47 +0200

    Remove cached ignored files and add test for mixed recipe

--- app/src/main/java/com/sipox11/recipe_testing/data/model/Recipe.java
+++ app/src/main/java/com/sipox11/recipe_testing/data/model/Recipe.java
@@ -1,8 +1,14 @@
 package com.sipox11.recipe_testing.data.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class Recipe {
+
+    private  static final String ID_PREFIX = "id=";
+    private static final String TITLE_PREFIX = "title=";
 
     public final String id;
     public final String title;
@@ -14,7 +20,31 @@ public class Recipe {
         this.description = description;
     }
 
-    public Recipe readFromStream(InputStream stream) {
-        return null;
+    public static Recipe readFromStream(InputStream stream) {
+        String id = null;
+        String title = null;
+        StringBuilder descBuilder = new StringBuilder();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+        try {
+            for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+                if(line.startsWith(ID_PREFIX)) {
+                    id = line.substring(ID_PREFIX.length());
+                } else if(line.startsWith(TITLE_PREFIX)) {
+                    title = line.substring(TITLE_PREFIX.length());
+                } else {
+                    if(descBuilder.length() > 0) {
+                        descBuilder.append("\n");
+                    }
+                    descBuilder.append(line);
+                }
+
+            }
+        } catch (IOException ioe) {
+            return null;
+        }
+        
+        return new Recipe(id, title, descBuilder.toString());
     }
 }
