a84e3c54b39dc4b399189193d9a8576f4e4d8033
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-04-21 23:29:06 +0200

    Add click to favorite test

--- app/src/androidTest/java/com/sipox11/recipe_testing/ui/recipe/RecipeActivityTest.java
+++ app/src/androidTest/java/com/sipox11/recipe_testing/ui/recipe/RecipeActivityTest.java
@@ -1,5 +1,6 @@
 package com.sipox11.recipe_testing.ui.recipe;
 
+import android.content.Intent;
 import android.support.test.rule.ActivityTestRule;
 
 import com.sipox11.recipe_testing.R;
@@ -8,8 +9,10 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.isSelected;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.CoreMatchers.not;
@@ -30,6 +33,21 @@ public class RecipeActivityTest {
         onView(withId(R.id.title)).check(matches(not(isDisplayed())));
     }
 
-    
+    @Test
+    public void clickToFavorite() {
+        Intent intent = new Intent();
+        intent.putExtra(RecipeActivity.KEY_ID, "creamed_carrots");
+        activityRule.launchActivity(intent);
+        // Verify that the title textview displays the correct text,
+        // that it is not selected initially
+        // and that when clicked, it changes to selected
+        onView(withId(R.id.title))
+                .check(matches(withText("Creamed Carrots")))
+                .check(matches(not(isSelected())))
+                .perform(click())
+                .check(matches(isSelected()));
+
+    }
+
 
 }
