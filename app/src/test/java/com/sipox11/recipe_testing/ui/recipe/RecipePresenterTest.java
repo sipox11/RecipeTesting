7ebb81635cc04865f121e6a1ba7c7a4d5652f46f
Jose Carlos Montero Gómez <sipox11@gmail.com>
2018-04-22 02:13:14 +0200

    Add Mockito dependency and mock the presenter constructor parameters as stubs

--- app/src/test/java/com/sipox11/recipe_testing/ui/recipe/RecipePresenterTest.java
+++ app/src/test/java/com/sipox11/recipe_testing/ui/recipe/RecipePresenterTest.java
@@ -1,7 +1,26 @@
 package com.sipox11.recipe_testing.ui.recipe;
 
+import com.sipox11.recipe_testing.data.local.Favorites;
+import com.sipox11.recipe_testing.data.local.RecipeStore;
+
+import org.junit.Before;
+import org.mockito.Mockito;
+
 import static org.junit.Assert.*;
 
 public class RecipePresenterTest {
+
+    private RecipeStore store;
+    private Favorites favorites;
+    private RecipeContract.View view;
+    private RecipePresenter presenter;
+
+    @Before
+    public void setup() {
+        store = Mockito.mock(RecipeStore.class);
+        favorites = Mockito.mock(Favorites.class);
+        view = Mockito.mock(RecipeContract.View.class);
+        presenter = new RecipePresenter(store, view, favorites );
+    }
 
 }
